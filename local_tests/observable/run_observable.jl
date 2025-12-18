79ddee60cfbfd912eb2ed3b6ee1baa8e78afc960
NishanRanabhat <nishanranabhat101@gmail.com>
2025-12-18 14:37:44 -0500

    local tests for testing simulation and analysis

---
+++ local_tests/observable/run_observable.jl
@@ -0,0 +1,24 @@
+using Pkg
+Pkg.activate(joinpath(@__DIR__, "..",".."))
+
+using TNCodebase
+using JSON
+
+# ============================================================================
+# LOAD CONFIGURATION
+# ============================================================================
+
+config_file = joinpath(@__DIR__, "analysis_config.json")
+config = JSON.parsefile(config_file)
+
+# Specify data directory relative to package root
+data_dir = joinpath(@__DIR__, "..","..","data")
+# Specify observable directory relative to package root
+obs_dir = joinpath(@__DIR__, "..","..","data_obs")
+
+obs_run_id, obs_run_dir = run_observable_calculation_from_config(
+    config, 
+    base_dir=data_dir,
+    obs_base_dir=obs_dir
+)
+
